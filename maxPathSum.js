165c3687942f4f64b0a74e5ef41fd77430ce9b1a
Priyanka Malviya <priyanka_malviya@icloud.com>
2019-10-16 20:20:01 -0700

    adding explanation for longest substring without repeating characters

--- maxPathSum.js
+++ maxPathSum.js
@@ -1,13 +1,27 @@
-var lengthOfLongestSubstring = function(s) {
+/*
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+*/
+
+var lengthOfLongestSubstring = function (s) {
 	let map = {}
 	let start = 0
 	let maxLen = 0
 	let arr = s.split('')
 
-	for (i=0; i < s.length; i++) {
+	for (i = 0; i < s.length; i++) {
 		let current = map[arr[i]]
 
-		if (current!=null && start <= current) {
+		if (current != null && start <= current) {
 			start = current + 1
 		} else {
 			maxLen = Math.max(maxLen, i - start + 1)
@@ -16,7 +30,7 @@ var lengthOfLongestSubstring = function(s) {
 
 	}
 
-return maxLen
+	return maxLen
 };
 
 console.log(lengthOfLongestSubstring("bbbbb"));
