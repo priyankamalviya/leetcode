aa4299487cb86466abd9ee7f2721e834e2f3e46a
Priyanka Malviya <priyanka_malviya@icloud.com>
2019-10-19 14:54:11 -0700

    longest mountain in array

--- longestMountain.js
+++ longestMountain.js
@@ -1,3 +1,16 @@
+/*
+Let's call any (contiguous) subarray B (of A) a mountain if the following properties hold:
+
+B.length >= 3
+There exists some 0 < i < B.length - 1 such that B[0] < B[1] < ... B[i-1] < B[i] > B[i+1] > ... > B[B.length - 1]
+(Note that B could be any subarray of A, including the entire array A.)
+
+Given an array A of integers, return the length of the longest mountain. 
+
+Return 0 if there is no mountain.
+
+*/
+
 var longestMountain = function (A) {
     let len = A.length,
         count = 0,
