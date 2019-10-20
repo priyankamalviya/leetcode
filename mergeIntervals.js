e0e4ca0c75cb9daf9a3bc80f9d912817c03d6c3f
Priyanka Malviya <priyanka_malviya@icloud.com>
2019-10-20 11:00:44 -0700

    merge interval

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -1,3 +1,15 @@
+/* Q 57. Insert Interval HARD
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+*/
+
 /**
  * Definition for an interval.
  * function Interval(start, end) {
@@ -10,26 +22,29 @@
  * @return {Interval[]}
  */
 
- function Interval(start, end) {
-    this.start = start;
-    this.end = end;
+function Interval(start, end) {
+  this.start = start;
+  this.end = end;
 }
-    
+
 const merge = intervals => {
-    intervals.sort((intervalA, intervalB) => intervalA.start - intervalB.start);
-    for (let i = 1; i < intervals.length; i++) {
-      const previousInterval = intervals[i - 1];
-      const currentInterval = intervals[i];
-      if (currentInterval.start <= previousInterval.end) {
-        intervals[i] = new Interval(
-          Math.min(previousInterval.start, currentInterval.start),
-          Math.max(previousInterval.end, currentInterval.end)
-        );
-        intervals[i - 1] = null;
-      }
+  intervals.sort((intervalA, intervalB) => intervalA.start - intervalB.start);
+  for (let i = 1; i < intervals.length; i++) {
+    const previousInterval = intervals[i - 1];
+    const currentInterval = intervals[i];
+    if (currentInterval.start <= previousInterval.end) {
+      intervals[i] = new Interval(
+        Math.min(previousInterval.start, currentInterval.start),
+        Math.max(previousInterval.end, currentInterval.end)
+      );
+      intervals[i - 1] = null;
     }
-    return intervals.filter(interval => interval);
-  };
+  }
+  return intervals.filter(interval => interval);
+};
 
 
-  console.log(merge([[4,5],[1,4]]));
+console.log(merge([
+  [4, 5],
+  [1, 4]
+]));
