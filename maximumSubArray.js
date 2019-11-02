7e953b53ca845c147f3d7bc26fe97c12092a579f
Priyanka Malviya <priyanka_malviya@icloud.com>
2019-11-02 10:13:31 -0700

    maximum sub array

--- maximumSubArray.js
+++ maximumSubArray.js
@@ -1,3 +1,12 @@
+/* Q.53 EASY
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+*/
 var maxSubArray = function (nums) {
     let dp = [];
     let max_sum = nums[0];
@@ -8,8 +17,8 @@ var maxSubArray = function (nums) {
             dp.push(sum);
             max_sum = max_sum > sum ? max_sum : sum;
         }
-    })
+    });
     return max_sum;
 };
 
-console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
