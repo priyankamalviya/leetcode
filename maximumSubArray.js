af197ac89d2da6c4ab14e01bff73e005375e12ff
Priyanka Malviya <priyanka_malviya@icloud.com>
2019-11-03 19:09:08 -0800

    arrays -> max subarray & remove duplicates

--- maximumSubArray.js
+++ maximumSubArray.js
@@ -6,19 +6,52 @@ Example:
 Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
+Time complexity = O(n);
 */
 var maxSubArray = function (nums) {
-    let dp = [];
-    let max_sum = nums[0];
+    let max_sum = 0,
+        dp = [];
     dp[0] = nums[0];
-    nums.forEach((num, index) => {
-        if (index != 0) {
-            let sum = Math.max(num, dp[index - 1] + num);
+    nums.map((num, index) => {
+        if (index !== 0) {
+            let sum = Math.max(num, num + dp[index - 1]);
             dp.push(sum);
-            max_sum = max_sum > sum ? max_sum : sum;
+            max_sum = Math.max(max_sum, sum);
         }
     });
     return max_sum;
 };
+
+// var maxSubArray = function (nums) {
+//     let n = nums.length - 1;
+//     if (n + 1 == 0) return 0;
+//     return maxSubArrayHelperFunction(nums, 0, 8);
+// };
+
+
+// var maxSubArrayHelperFunction = function (arr, left, right) {
+//     if (right === left) return arr[left];
+//     console.log("right->", right)
+//     let mid = left + right / 2;
+//     let leftSum = maxSubArrayHelperFunction(arr, left, mid);
+//     let rightSum = maxSubArrayHelperFunction(arr, mid + 1, right);
+//     let leftMax = arr[mid];
+//     let rightMax = arr[mid + 1];
+//     let temp = 0;
+//     for (let i = mid; i >= left; i--) {
+//         temp += arr[i];
+//         if (temp > leftMax) leftMax = temp;
+//     }
+
+//     temp = 0;
+
+//     for (let i = mid + 1; i < right; i++) {
+//         temp += arr[i];
+//         if (temp > rightMax) rightMax = temp;
+//     }
+
+//     return Math.max(leftSum, rightSum, rightMax + leftMax);
+// };
+
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,11 +1,29 @@
-var removeDuplicates = function(nums) {
-        //return [...new Set(nums)];
-        let i = 0, j = 1;
-        while (j < nums.length) 
-            if (nums[i] !== nums[j++]) 
-                 nums[++i] = nums[j - 1];
-        return i + 1;
-    
- };
+// var removeDuplicates = function (nums) {
+//     //return [...new Set(nums)];
+//     let i = 0,
+//         j = 1;
+//     while (j < nums.length)
+//         if (nums[i] !== nums[j++])
+//             nums[++i] = nums[j - 1];
+//     return i + 1;
 
- console.log(removeDuplicates([1,1,2]));
+// };
+
+let removeDuplicates = function (nums) {
+    let i = 0,
+        len = nums.length,
+        arr = [];
+    arr.push(nums[0]);
+    while (i < len && typeof (nums[i + 1]) !== 'undefined') {
+        if (nums[i] !== nums[i + 1]) {
+            arr.push(nums[i + 1]);
+        } else {
+            i++;
+            continue;
+        }
+        i++;
+    }
+    return arr;
+}
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
