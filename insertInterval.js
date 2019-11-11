9013f89f5207a8edcbaa9bb3f2e960d54ffbce1b
Priyanka Malviya <priyanka_malviya@icloud.com>
2019-11-10 21:48:38 -0800

    adding description to insert interval

--- insertInterval.js
+++ insertInterval.js
@@ -1,3 +1,14 @@
+/*
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+*/
+
 var insert = function (intervals, newInterval) {
     const output = [];
     let ptr = newInterval;
