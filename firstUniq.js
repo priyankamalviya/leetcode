9193ad28535fe5055f1120a335c871acd82762c0
Priyanka Malviya <priyanka_malviya@icloud.com>
2019-11-13 22:05:16 -0800

    first unique character explanation

--- firstUniq.js
+++ firstUniq.js
@@ -1,15 +1,27 @@
-var firstUniqChar = function(s) {
+/*
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+*/
+
+var firstUniqChar = function (s) {
     let map = {};
 
-    for(let i = 0; i < s.length; i++) {
-    if (map[s[i]] || map[s[i]] === 0) {
-        map[s[i]] = -1
-    } else {
-        map[s[i]]= i
+    for (let i = 0; i < s.length; i++) {
+        if (map[s[i]] || map[s[i]] === 0) {
+            map[s[i]] = -1
+        } else {
+            map[s[i]] = i
+        }
     }
-}
 
- let result = Object.values(map).filter(val => val >= 0)
+    let result = Object.values(map).filter(val => val >= 0)
     if (result.length === 0) {
         return -1
     } else {
