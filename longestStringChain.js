16b02e1017e567295318941cddfbbcd781fcef40
Priyanka Malviya <priyanka_malviya@icloud.com>
2020-02-17 12:47:47 -0800

    longest string chain

---
+++ longestStringChain.js
@@ -0,0 +1,15 @@
+var longestStrChain = function (words) {
+    let DP = {};
+    for (let word of words) {
+        let longest = 0;
+        for (let i = 0; i < words.length; i++) {
+            let prefix = word.slice(0, i) + word.slice(i + 1); // .slice(i+1) takes last char, for ex: bca will give 3 prefixes, bc, ba and ca
+            longest = Math.max(longest, (DP[prefix] || 0) + 1);
+        }
+        DP[word] = longest;
+    }
+    return Math.max(...Object.values(DP));
+};
+
+
+console.log(longestStrChain(["a", "b", "ba", "bca", "bda", "bdca"]));
